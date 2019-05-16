42a78c0c5de0a217b6bdebec2caaa875387c00ac
bruno99figueiredo <49922598+bruno99figueiredo@users.noreply.github.com>
2019-05-17 00:08:32 +0100

    Mudanças para o trabalho de Es1

--- src/Main.java
+++ src/Main.java
@@ -16,8 +16,8 @@ public class Main {
         /*Novas Classes*/
         //
         Leitor leitor= new Leitor("Bruno","ativo", "aluno");
-        TipoSala tipoSala=new TipoSala("leitura",3);
-        Sala sala =new Sala("livre", tipoSala);
+        TipoSala tipoSala=new TipoSala("leitura",3,LocalDate.now());
+        Sala sala =new Sala("livre", tipoSala,1);
         SistemaLimpeza sistemaLimpeza = new SistemaLimpeza("Ajax",3, LocalDate.now(),sala);
         GestaoLeitura gestaoLeitura= new GestaoLeitura(sala,"Romance", LocalDate.now(), 3, leitor);
         //
