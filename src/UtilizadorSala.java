e248c677f6ff1d5ef1220b85afa103791d550550
bruno99figueiredo <49922598+bruno99figueiredo@users.noreply.github.com>
2019-05-17 16:56:01 +0100

    Classe extra para o Sistema de Aluguer de Salas

---
+++ src/UtilizadorSala.java
@@ -0,0 +1,17 @@
+public class UtilizadorSala {
+    private String nome;
+    private String descricao;
+
+    public UtilizadorSala(String nome, String descricao) {
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
