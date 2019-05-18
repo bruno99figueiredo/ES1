d6902ef4755ab359a085b662e0cc9a45ba1fafff
bruno99figueiredo <49922598+bruno99figueiredo@users.noreply.github.com>
2019-05-18 16:37:52 +0100

    Classe criada para a Gestão das Sessões de Leitura

--- src/GestaoLeitura.java
+++ src/GestaoLeitura.java
@@ -2,13 +2,15 @@ import java.time.LocalDate;
 
 public class GestaoLeitura {
     private Sala sala;
+    private Livro livro;
     private String tipo_livro;
     private LocalDate data;
     private int nr_horas;
     private Leitor leitor;
 
-    public GestaoLeitura(Sala sala, String tipo_livro, LocalDate data, int nr_horas, Leitor leitor) {
+    public GestaoLeitura(Sala sala, Livro livro, String tipo_livro, LocalDate data, int nr_horas, Leitor leitor) {
         this.sala = sala;
+        this.livro = livro;
         this.tipo_livro = tipo_livro;
         this.data = data;
         this.nr_horas = nr_horas;
@@ -17,6 +19,10 @@ public class GestaoLeitura {
 
     public Sala getSala() {
         return sala;
+    }
+
+    public Livro getLivro() {
+        return livro;
     }
 
     public String getTipo_livro() {
