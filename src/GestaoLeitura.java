7dbf57670a9ed911b323b09fe10e0d722397a4e9
bruno99figueiredo <49922598+bruno99figueiredo@users.noreply.github.com>
2019-05-24 00:23:12 +0100

    Classe de Gestão de Sessões de Leitura para o trabalho de Es1

--- src/GestaoLeitura.java
+++ src/GestaoLeitura.java
@@ -5,14 +5,16 @@ public class GestaoLeitura {
     private Livro livro;
     private String tipo_livro;
     private LocalDate data;
+    private int hora_entrada;
     private int nr_horas;
     private Leitor leitor;
 
-    public GestaoLeitura(Sala sala, Livro livro, String tipo_livro, LocalDate data, int nr_horas, Leitor leitor) {
+    public GestaoLeitura(Sala sala, Livro livro, String tipo_livro, LocalDate data, int hora_entrada, int nr_horas, Leitor leitor) {
         this.sala = sala;
         this.livro = livro;
         this.tipo_livro = tipo_livro;
         this.data = data;
+        this.hora_entrada = hora_entrada;
         this.nr_horas = nr_horas;
         this.leitor = leitor;
     }
@@ -31,6 +33,10 @@ public class GestaoLeitura {
 
     public LocalDate getData() {
         return data;
+    }
+
+    public int getHora_entrada() {
+        return hora_entrada;
     }
 
     public int getNr_horas() {
