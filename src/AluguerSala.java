835e3b131c1b782e6cfd4f8ecb81aa716ced9efa
bruno99figueiredo <49922598+bruno99figueiredo@users.noreply.github.com>
2019-05-24 00:22:34 +0100

    Classe de Sistema de Aluguer de Salas para o trabalho de Es1

--- src/AluguerSala.java
+++ src/AluguerSala.java
@@ -2,12 +2,14 @@ import java.time.LocalDate;
 
 public class AluguerSala {
     private UtilizadorSala utilizadorSala;
+    private int hora_entrada;
     private int tempo_utilizacao;
     private LocalDate data;
     private Sala sala;
 
-    public AluguerSala(UtilizadorSala utilizadorSala, int tempo_utilizacao, LocalDate data, Sala sala) {
+    public AluguerSala(UtilizadorSala utilizadorSala, int hora_entrada, int tempo_utilizacao, LocalDate data, Sala sala) {
         this.utilizadorSala = utilizadorSala;
+        this.hora_entrada = hora_entrada;
         this.tempo_utilizacao = tempo_utilizacao;
         this.data = data;
         this.sala = sala;
@@ -15,6 +17,10 @@ public class AluguerSala {
 
     public UtilizadorSala getUtilizadorSala() {
         return utilizadorSala;
+    }
+
+    public int getHora_entrada() {
+        return hora_entrada;
     }
 
     public int getTempo_utilizacao() {
